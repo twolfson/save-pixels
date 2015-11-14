8019ae0ac077c4d1c3274f2b05e803a711819427
Todd Wolfson <todd@twolfson.com>
2015-11-14 13:54:07 -0600

    Moved to granular checkerboard

--- test/test.js
+++ test/test.js
@@ -287,9 +287,11 @@ tap("save-pixels saving 2 jpeg images with the different qualities are different
 
   for(var i=0; i<64; ++i) {
     for(var j=0; j<64; ++j) {
-      x.set(i, j, 0, i)
-      x.set(i, j, 0, j)
-      x.set(i, j, 0, i+2*j)
+      // 1x1 black and white checkerboard pattern
+      var value = (i % 2 === 0 && j % 2 === 0) ? 255 : 0
+      x.set(i, j, 0, value)
+      x.set(i, j, 0, value)
+      x.set(i, j, 0, value)
     }
   }
   writePixels(t, x, lowQualityFilepath, "jpeg", {quality: 1}, function(err) {
